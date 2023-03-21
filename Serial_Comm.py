09b6635ffce8ac77fe26e3c8d594ce48c5b3a483
Teore123 <ptj971226@gmail.com>
2023-03-21 12:22:22 +0900

    edit send str

--- Serial_Comm.py
+++ Serial_Comm.py
@@ -4,7 +4,6 @@ import numpy as np
 import threading
 from multiprocessing import Queue
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QMessageBox
 from Steer_Status import Steer_Status
 
 class Serial_Comm(QObject):
@@ -39,8 +38,6 @@ class Serial_Comm(QObject):
 
     def read_from_arduino(self, port, baud):
         self.ser = self.connect_serial(port, baud)
-        # self.ser.close()
-        # self.ser.open()
         while self.ser.is_open:
             if self.ser.readable():
                 try:
@@ -69,17 +66,16 @@ class Serial_Comm(QObject):
         
     def cal_bytes(self, data):
         return np.array((data[1] << 8) | data[0]).astype(np.int16)
-    
-    def send_str(self, mode, speed, cycle, offset, amp):
-        self.ser.write(bytes([mode, speed, cycle, offset, amp]))
+
+    def send_str(self, mode, data): # data = [speed, cycle, offset, amp]
+        self.ser.write(bytes([mode, data[0],data[1],data[2],data[3]]))
         
     def send_mode(self, mode):
-        self.send_str(mode, 0, 0, 0, 0)
+        self.send_str(mode, [0, 0, 0, 0])
 
 def main():
     status = Steer_Status()
     test = Serial_Comm(status)
-    # ser = test.connect_serial()
     test.read_from_arduino()
 
 if __name__ == '__main__':
